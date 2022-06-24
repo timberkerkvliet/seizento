b48788e7d01de27d956418ae40d2abe3af27fb5e
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-06-24 10:36:29 +0200

    add a test

--- tests/test_expressions/test_reference.py
+++ tests/test_expressions/test_reference.py
@@ -1,6 +1,6 @@
 from unittest import IsolatedAsyncioTestCase
 
-from seizento.controllers.exceptions import Forbidden
+from seizento.controllers.exceptions import Forbidden, NotFound
 from tests.test_client import UnitTestClient
 
 
@@ -16,6 +16,13 @@ class TestReference(IsolatedAsyncioTestCase):
 
         response = await self.test_client.get('/evaluation/')
         self.assertEqual(response, [1, 1])
+
+    async def test_non_existing_key(self):
+        await self.test_client.set('/schema/', {'type': 'array', 'items': {'type': 'integer'}})
+        await self.test_client.set('/expression/', ['{/1}'])
+
+        with self.assertRaises(NotFound):
+            await self.test_client.get('/evaluation/')
 
     async def test_reference_with_wrong_type(self):
         await self.test_client.set(
