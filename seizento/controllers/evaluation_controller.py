5ae0eaf3a1000609e4a90d10529c82e633095012
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-06-24 13:57:35 +0200

    move controller

--- seizento/controllers/evaluation_controller.py
+++ seizento/controllers/evaluation_controller.py
@@ -1,10 +1,33 @@
-from typing import Dict, Any
+from dataclasses import dataclass
+from typing import Dict, Any, Optional
 
 from seizento.controllers.exceptions import NotFound
-from seizento.controllers.expression_tools import find_nearest_expression
 from seizento.domain.expression import Expression
 from seizento.path import Path
 from seizento.repository import Repository
+
+
+@dataclass(frozen=True)
+class NearestExpressionResult:
+    expression: Expression
+    path: Path
+
+
+async def find_nearest_expression(repository: Repository, path: Path) -> Optional[NearestExpressionResult]:
+    current_path = path
+    while True:
+        expression = await repository.get_expression(current_path)
+        if expression is not None:
+            return NearestExpressionResult(
+                expression=expression,
+                path=current_path
+            )
+
+        if path.empty:
+            return None
+
+        path = path.remove_last()
+        continue
 
 
 async def evaluate(path: Path, repository: Repository) -> Any:
