841ed4618b1b807d41d9a1a9797a36c43508feb7
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-06-27 11:30:44 +0200

    another test

--- tests/test_interface/test_types/test_schema_change.py
+++ tests/test_interface/test_types/test_schema_change.py
@@ -49,3 +49,23 @@ class TestSchemaChange(IsolatedAsyncioTestCase):
 
         self.assertDictEqual(response, new_schema)
 
+    async def test_missing_value_after_change(self):
+        await self.test_client.set(
+            '/schema/',
+            {'type': 'object', 'properties': {'a': {'type': 'integer'}}}
+        )
+        await self.test_client.set('/expression/',  {'a': 900})
+
+        new_schema = {
+            'type': 'object',
+            'properties': {
+                'a': {'type': 'integer'},
+                'b': {'type': 'integer'}
+            }
+        }
+
+        await self.test_client.set('/schema/', new_schema)
+
+        response = await self.test_client.get('/evaluation')
+
+        self.assertDictEqual(response, {'a': 900})
