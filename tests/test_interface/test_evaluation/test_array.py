06a9f9fc4618243f2dc458337f9b6652d387bb0d
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-03 13:32:04 +0200

    array eval tests

--- tests/test_interface/test_evaluation/test_array.py
+++ tests/test_interface/test_evaluation/test_array.py
@@ -13,3 +13,18 @@ class TestArray(IsolatedAsyncioTestCase):
         await self.test_client.set('/expression/', [1, 2, 3, 4])
         response = await self.test_client.get('/evaluation/')
         self.assertEqual(response, [1, 2, 3, 4])
+
+    async def test_nested_arrays(self):
+        await self.test_client.set(
+            '/schema/',
+            {'type': 'array', 'items': {'type': 'array', 'items': {'type': 'integer'}}}
+        )
+        await self.test_client.set('/expression/', [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4]])
+        response = await self.test_client.get('/evaluation/')
+        self.assertEqual(response, [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4]])
+
+    async def test_item_evaluation(self):
+        await self.test_client.set('/schema/', {'type': 'array', 'items': {'type': 'integer'}})
+        await self.test_client.set('/expression/', [1, 2, 3, 4])
+        response = await self.test_client.get('/evaluation/1')
+        self.assertEqual(response, 2)
