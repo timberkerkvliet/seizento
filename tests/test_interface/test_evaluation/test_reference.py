f652c5f1b413340ca3396d51b1b19f5e9de5bfae
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-03 13:21:09 +0200

    start reorganising test

--- tests/test_interface/test_evaluation/test_reference.py
+++ tests/test_interface/test_evaluation/test_reference.py
@@ -22,22 +22,6 @@ class TestReference(IsolatedAsyncioTestCase):
         with self.assertRaises(NotFound):
             await self.test_client.get('/evaluation/')
 
-    async def test_reference_with_wrong_type(self):
-        await self.test_client.set(
-            '/schema/',
-            {
-                'type': 'object',
-                'properties': {
-                    'a': {'type': 'integer'},
-                    'b': {'type': 'string'}
-                }
-            }
-        )
-        await self.test_client.set('/expression', {'a': 5})
-
-        with self.assertRaises(Forbidden):
-            await self.test_client.set('/expression/b', '{/a}')
-
     async def test_object_reference(self):
         await self.test_client.set(
             '/schema/',
@@ -76,31 +60,3 @@ class TestReference(IsolatedAsyncioTestCase):
 
         response = await self.test_client.get('/evaluation/')
         self.assertEqual(response, [1, 1, 1])
-
-    async def test_cycle_of_three(self):
-        await self.test_client.set('/schema/', {'type': 'array', 'items': {'type': 'integer'}})
-
-        await self.test_client.set('/expression/', ['{/1}', '{/2}'])
-
-        with self.assertRaises(Forbidden):
-            await self.test_client.set('/expression/2', '{/0}')
-
-    async def test_self_reference(self):
-        await self.test_client.set('/schema/', {'type': 'array', 'items': {'type': 'integer'}})
-
-        with self.assertRaises(Forbidden):
-            await self.test_client.set('/expression', ['{/0}'])
-
-    async def test_self_reference_did_not_change_anything(self):
-        await self.test_client.set('/schema/', {'type': 'array', 'items': {'type': 'integer'}})
-
-        await self.test_client.set('/expression', [1])
-
-        try:
-            await self.test_client.set('/expression', ['{/0}'])
-        except Forbidden:
-            pass
-
-        response = await self.test_client.get('/expression')
-
-        self.assertEqual(response, [1])
