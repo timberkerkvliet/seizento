b5a50ffefbdeb1449bdf5f9499663e8df487087d
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-03 13:24:06 +0200

    split string

---
+++ tests/test_interface/test_evaluation/test_string.py
@@ -0,0 +1,40 @@
+from unittest import IsolatedAsyncioTestCase
+
+from seizento.controllers.exceptions import Forbidden, NotFound
+from tests.test_interface.test_client import UnitTestClient
+
+
+class TestString(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self) -> None:
+        self.test_client = UnitTestClient()
+
+    async def test_set_and_get_literal_with_escaped_brackets(self):
+        await self.test_client.set('/schema/', {'type': 'string'})
+        await self.test_client.set(
+            '/expression/',
+            'a literal {{ string'
+        )
+        response = await self.test_client.get('/evaluation/')
+        self.assertEqual(response, 'a literal { string')
+
+    async def test_set_and_get_literal_with_double_escaped_brackets(self):
+        await self.test_client.set('/schema/', {'type': 'string'})
+        await self.test_client.set(
+            '/expression/',
+            '{{a}}'
+        )
+        response = await self.test_client.get('/evaluation/')
+        self.assertEqual(response, '{a}')
+
+    async def test_set_and_evaluate_literal(self):
+        await self.test_client.set('/schema/', {'type': 'string'})
+        await self.test_client.set('/expression/', 'a literal string')
+
+        response = await self.test_client.get('/evaluation/')
+        self.assertEqual(response, 'a literal string')
+
+    async def test_missing_value(self):
+        await self.test_client.set('/schema/', {'type': 'string'})
+
+        with self.assertRaises(NotFound):
+            await self.test_client.get('/evaluation')
--- tests/test_interface/test_expression/test_string.py
+++ tests/test_interface/test_expression/test_string.py
@@ -17,38 +17,7 @@ class TestString(IsolatedAsyncioTestCase):
         response = await self.test_client.get('/expression/')
         self.assertEqual(response, 'a literal string')
 
-    async def test_set_and_get_literal_with_escaped_brackets(self):
-        await self.test_client.set('/schema/', {'type': 'string'})
-        await self.test_client.set(
-            '/expression/',
-            'a literal {{ string'
-        )
-        response = await self.test_client.get('/evaluation/')
-        self.assertEqual(response, 'a literal { string')
-
-    async def test_set_and_get_literal_with_double_escaped_brackets(self):
-        await self.test_client.set('/schema/', {'type': 'string'})
-        await self.test_client.set(
-            '/expression/',
-            '{{a}}'
-        )
-        response = await self.test_client.get('/evaluation/')
-        self.assertEqual(response, '{a}')
-
-    async def test_set_and_evaluate_literal(self):
-        await self.test_client.set('/schema/', {'type': 'string'})
-        await self.test_client.set('/expression/', 'a literal string')
-
-        response = await self.test_client.get('/evaluation/')
-        self.assertEqual(response, 'a literal string')
-
     async def test_set_wrong_literal(self):
         await self.test_client.set('/schema/', {'type': 'string'})
         with self.assertRaises(Forbidden):
             await self.test_client.set('/expression/', 9000)
-
-    async def test_missing_value(self):
-        await self.test_client.set('/schema/', {'type': 'string'})
-
-        with self.assertRaises(NotFound):
-            await self.test_client.get('/evaluation')
