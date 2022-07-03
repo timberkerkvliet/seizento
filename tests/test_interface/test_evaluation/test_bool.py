631306a03ba5014ec88f17957374075584bfdc12
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-03 17:26:33 +0200

    bool tests

--- tests/test_interface/test_evaluation/test_bool.py
+++ tests/test_interface/test_evaluation/test_bool.py
@@ -8,9 +8,22 @@ class TestBool(IsolatedAsyncioTestCase):
     async def asyncSetUp(self) -> None:
         self.test_client = UnitTestClient()
 
-    async def test_set_and_get_literal_with_escaped_brackets(self):
+    async def test_set_true(self):
         await self.test_client.set('/schema/', {'type': 'boolean'})
         await self.test_client.set('/expression/', True)
 
         response = await self.test_client.get('/evaluation/')
         self.assertEqual(response, True)
+
+    async def test_set_false(self):
+        await self.test_client.set('/schema/', {'type': 'boolean'})
+        await self.test_client.set('/expression/', False)
+
+        response = await self.test_client.get('/evaluation/')
+        self.assertEqual(response, False)
+
+    async def test_not_set(self):
+        await self.test_client.set('/schema/', {'type': 'boolean'})
+
+        with self.assertRaises(NotFound):
+            await self.test_client.get('/evaluation')
