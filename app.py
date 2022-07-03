7be8598b7870266f68b99d4321959df608633a13
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-03 23:21:58 +0200

    starlette

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 from fastapi import FastAPI
 
-from seizento.adapters.starlette_adapter import FastAPIRequestHandler
+from seizento.adapters.starlette_adapter import StarletteAdapter
 from seizento.controllers.resource_controller import ResourceController
 from seizento.repository import Repository
 from tests.test_interface.test_client import FakeDataTreeStore
@@ -10,7 +10,7 @@ from tests.test_interface.test_client import FakeDataTreeStore
 app = FastAPI()
 store = FakeDataTreeStore()
 
-handler = FastAPIRequestHandler(
+handler = StarletteAdapter(
     resource_controller=ResourceController(
         repository_factory=lambda: Repository(store.get_transaction()),
         user_id=UUID()
