777da61827deeebac8ac0a3872209e6eb2a3c7d4
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-03 23:14:53 +0200

    fastapi

--- seizento/adapters/fastapi_adapter.py
+++ seizento/adapters/fastapi_adapter.py
@@ -1,24 +1,39 @@
-import json
-
 from fastapi import FastAPI
 from starlette.requests import Request
+from starlette.responses import Response
 
+from seizento.controllers.exceptions import NotFound, BadRequest, Forbidden
 from seizento.controllers.resource_controller import ResourceController
+
+
+class FastAPIRequestHandler:
+    def __init__(self, resource_controller: ResourceController):
+        self._resource_controller = resource_controller
+
+    async def handle(self, request: Request) -> Response:
+        resource = request.url.path
+        try:
+            if request.method == 'get':
+                result = await self._resource_controller.get(resource=resource)
+            elif request.method == 'put':
+                result = await self._resource_controller.set(resource=resource, data=request.json())
+            elif request.method == 'delete':
+                result = await self._resource_controller.delete(resource=resource)
+            else:
+                return Response(status_code=405)
+        except NotFound:
+            return Response(status_code=404)
+        except BadRequest:
+            return Response(status_code=400)
+        except Forbidden:
+            return Response(status_code=401)
+
+        return Response(result)
 
 
 def get_app(resource_controller: ResourceController) -> FastAPI:
     app = FastAPI()
-
-    @app.get('/{resource_path:path}')
-    async def get(resource_path: str):
-        return resource_controller.get(resource_path)
-
-    @app.put('/{resource_path:path}')
-    async def put(resource_path: str, request: Request):
-        return resource_controller.set(resource_path, data=json.loads(await request.body()))
-
-    @app.delete('/{resource_path:path}')
-    async def delete(resource_path: str):
-        return resource_controller.delete(resource_path)
+    handler = FastAPIRequestHandler(resource_controller=resource_controller)
+    app.add_route(path='/', route=handler.handle)
 
     return app
