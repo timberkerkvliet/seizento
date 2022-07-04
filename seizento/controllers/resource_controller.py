05dbe38110ea7310f76cb6a669ee45b055b6d2a5
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-04 22:17:57 +0200

    tokens

--- seizento/controllers/resource_controller.py
+++ seizento/controllers/resource_controller.py
@@ -1,27 +1,30 @@
 from typing import Dict, Any, Callable
-from uuid import UUID
+
+import jwt
 
 from seizento.controllers.evaluation_controller import EvaluationController
 from seizento.controllers.exceptions import BadRequest, Unauthorized
 from seizento.controllers.expression_controller import ExpressionController
 from seizento.controllers.schema_controller import SchemaController
-from seizento.path import EMPTY_PATH
 from seizento.repository import Repository, DataTreeStoreTransaction, RestrictedDataTreeStoreTransaction, Restricted
 from seizento.serializers.path_serializer import parse_path
+from seizento.serializers.user_serializer import parse_access_rights
 from seizento.user import AccessRights
 
 
 class ResourceController:
     def __init__(
         self,
-        transaction_factory: Callable[[], DataTreeStoreTransaction]
+        transaction_factory: Callable[[], DataTreeStoreTransaction],
+        token_secret: str
     ):
         self._transaction_factory = transaction_factory
+        self._token_secret = token_secret
 
-    def _repository_factory(self) -> Repository:
+    def _repository_factory(self, token: str) -> Repository:
         return Repository(
             transaction=RestrictedDataTreeStoreTransaction(
-                access_rights=AccessRights(read_access={EMPTY_PATH}, write_access={EMPTY_PATH}),
+                access_rights=self._get_access_rights(token=token),
                 wrapped=self._transaction_factory()
             )
         )
@@ -52,8 +55,16 @@ class ResourceController:
 
         raise BadRequest
 
-    async def get(self, resource: str) -> Dict:
-        async with self._repository_factory() as repository:
+    def _get_access_rights(self, token: str) -> AccessRights:
+        try:
+            return parse_access_rights(
+                jwt.decode(jwt=token, key=self._token_secret, algorithms='HS256')
+            )
+        except Exception as e:
+            raise Unauthorized
+
+    async def get(self, resource: str, token: str) -> Dict:
+        async with self._repository_factory(token) as repository:
             controller = self._get_controller(resource=resource, repository=repository)
 
             try:
@@ -61,8 +72,8 @@ class ResourceController:
             except Restricted as e:
                 raise Unauthorized from e
 
-    async def set(self, resource: str, data: Any) -> None:
-        async with self._repository_factory() as repository:
+    async def set(self, resource: str, data: Any, token: str) -> None:
+        async with self._repository_factory(token) as repository:
             controller = self._get_controller(resource=resource, repository=repository)
 
             try:
@@ -70,8 +81,8 @@ class ResourceController:
             except Restricted as e:
                 raise Unauthorized from e
 
-    async def delete(self, resource: str) -> None:
-        async with self._repository_factory() as repository:
+    async def delete(self, resource: str, token: str) -> None:
+        async with self._repository_factory(token) as repository:
             controller = self._get_controller(resource=resource, repository=repository)
 
             try:
--- tests/unit/unit_test_client.py
+++ tests/unit/unit_test_client.py
@@ -1,5 +1,10 @@
+import jwt
+
 from seizento.adapters.in_memory_data_tree_store import InMemoryDataTreeStore
 from seizento.controllers.resource_controller import ResourceController
+from seizento.path import EMPTY_PATH
+from seizento.serializers.user_serializer import serialize_access_rights
+from seizento.user import AccessRights
 
 
 class UnitTestClient:
@@ -8,14 +13,21 @@ class UnitTestClient:
     def __init__(self):
         store = InMemoryDataTreeStore()
         self.controller = ResourceController(
-            transaction_factory=lambda: store.get_transaction()
+            transaction_factory=lambda: store.get_transaction(),
+            token_secret='test-secret'
+        )
+        self.admin_token = jwt.encode(
+            payload=serialize_access_rights(
+                AccessRights(read_access={EMPTY_PATH}, write_access={EMPTY_PATH})
+            ),
+            key='test-secret'
         )
 
     async def get(self, resource: str):
-        return await self.controller.get(resource=resource)
+        return await self.controller.get(resource=resource, token=self.admin_token)
 
     async def set(self, resource: str, data):
-        await self.controller.set(resource=resource, data=data)
+        await self.controller.set(resource=resource, data=data, token=self.admin_token)
 
     async def delete(self, resource: str) -> None:
-        await self.controller.delete(resource=resource)
+        await self.controller.delete(resource=resource, token=self.admin_token)
