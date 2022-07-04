e6f37a9c641e735c3b3fbd06dff98d2e7e0e15fd
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-04 09:13:50 +0200

    json response

--- seizento/adapters/starlette_request_handler.py
+++ seizento/adapters/starlette_request_handler.py
@@ -1,7 +1,7 @@
 import json
 
 from starlette.requests import Request
-from starlette.responses import Response
+from starlette.responses import JSONResponse
 
 from seizento.controllers.exceptions import NotFound, BadRequest, Forbidden
 from seizento.controllers.resource_controller import ResourceController
@@ -11,7 +11,7 @@ class StarletteRequestHandler:
     def __init__(self, resource_controller: ResourceController):
         self._resource_controller = resource_controller
 
-    async def handle(self, request: Request) -> Response:
+    async def handle(self, request: Request) -> JSONResponse:
         resource = request.url.path
         try:
             if request.method == 'GET':
@@ -22,12 +22,12 @@ class StarletteRequestHandler:
             elif request.method == 'DELETE':
                 result = await self._resource_controller.delete(resource=resource)
             else:
-                return Response(status_code=405)
+                return JSONResponse(content='', status_code=405)
         except NotFound as e:
-            return Response(content=str(e), status_code=404)
+            return JSONResponse(content=str(e), status_code=404)
         except BadRequest as e:
-            return Response(content=str(e), status_code=400)
+            return JSONResponse(content=str(e), status_code=400)
         except Forbidden as e:
-            return Response(content=str(e), status_code=401)
+            return JSONResponse(content=str(e), status_code=401)
 
-        return Response(content=json.dumps(result), status_code=200)
+        return JSONResponse(content=result, status_code=200)
