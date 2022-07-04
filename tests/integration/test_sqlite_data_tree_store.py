dc8dadc64e718974a928b20ec7930523dd7f546c
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-04 19:15:55 +0200

    second test

--- tests/integration/test_sqlite_data_tree_store.py
+++ tests/integration/test_sqlite_data_tree_store.py
@@ -2,7 +2,7 @@ from unittest import IsolatedAsyncioTestCase
 
 from seizento.adapters.sqllite_data_tree_store import SQLiteDataTreeStore
 from seizento.data_tree import DataTree
-from seizento.path import EMPTY_PATH, LiteralComponent
+from seizento.path import EMPTY_PATH, LiteralComponent, Path
 
 
 class TestSQLiteDataTreeStore(IsolatedAsyncioTestCase):
@@ -13,7 +13,7 @@ class TestSQLiteDataTreeStore(IsolatedAsyncioTestCase):
         async with transaction:
             await transaction.delete_tree(EMPTY_PATH)
 
-    async def test_first_case(self):
+    async def test_get_root(self):
         tree = DataTree(
             root_data={'a': 1},
             subtrees={
@@ -33,3 +33,36 @@ class TestSQLiteDataTreeStore(IsolatedAsyncioTestCase):
             )
 
         self.assertEqual(tree, result)
+
+    async def test_add_child(self):
+        tree = DataTree(
+            root_data={'a': 1},
+            subtrees={
+                LiteralComponent('hey'): DataTree(root_data={'b': 2})
+            }
+        )
+
+        async with self.store.get_transaction() as transaction:
+            await transaction.set_tree(
+                path=EMPTY_PATH,
+                tree=tree
+            )
+
+        async with self.store.get_transaction() as transaction:
+            await transaction.set_tree(
+                path=Path(components=(LiteralComponent('hoi'),)),
+                tree=DataTree(root_data={'hoi': 'hoi'})
+            )
+
+        async with self.store.get_transaction() as transaction:
+            result = await transaction.get_tree(
+                path=EMPTY_PATH
+            )
+
+        self.assertEqual(DataTree(
+            root_data={'a': 1},
+            subtrees={
+                LiteralComponent('hey'): DataTree(root_data={'b': 2}),
+                LiteralComponent('hoi'): DataTree(root_data={'hoi': 'hoi'})
+            }
+        ), result)
