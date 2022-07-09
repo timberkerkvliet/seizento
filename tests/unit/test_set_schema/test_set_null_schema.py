ae229b5986dca1c532efb905832d76c7360f23af
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-09 19:02:36 +0200

    some more tests

--- tests/unit/test_set_schema/test_set_null_schema.py
+++ tests/unit/test_set_schema/test_set_null_schema.py
@@ -13,3 +13,33 @@ class TestSetNullSchema(IsolatedAsyncioTestCase):
 
         response = await self.test_client.get('/schema/')
         self.assertDictEqual(response, {'type': 'null'})
+
+    async def test_set_null_schema_if_null_is_set(self):
+        await self.test_client.set('/schema/', {'type': ['null', 'string']})
+        await self.test_client.set('/expression/', None)
+
+        try:
+            await self.test_client.get('/schema/')
+        except Forbidden:
+            self.fail()
+
+    async def test_set_null_schema_if_literal_string_is_set(self):
+        await self.test_client.set('/schema/', {'type': ['null', 'string']})
+        await self.test_client.set('/expression/', 'a string')
+
+        with self.assertRaises(Forbidden):
+            await self.test_client.set('/schema/', {'type': 'null'})
+
+    async def test_set_null_schema_if_object_schema_is_set(self):
+        await self.test_client.set('/schema/', {'type': 'object'})
+        await self.test_client.set('/expression/', {})
+
+        with self.assertRaises(Forbidden):
+            await self.test_client.set('/schema/', {'type': 'null'})
+
+    async def test_set_null_schema_if_array_schema_is_set(self):
+        await self.test_client.set('/schema/', {'type': 'array', 'items': {'type': 'string'}})
+        await self.test_client.set('/expression/', [])
+
+        with self.assertRaises(Forbidden):
+            await self.test_client.set('/schema/', {'type': 'null'})
