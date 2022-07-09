2ee11f7339b9202e2f2b1860601cc09281a1912c
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-09 18:00:40 +0200

    renaming

--- tests/unit/test_set_schema/test_set_null_schema.py
+++ tests/unit/test_set_schema/test_set_null_schema.py
@@ -4,15 +4,12 @@ from seizento.controllers.exceptions import Forbidden
 from tests.unit.unit_test_client import UnitTestClient
 
 
-class TestString(IsolatedAsyncioTestCase):
+class TestSetNullSchema(IsolatedAsyncioTestCase):
     async def asyncSetUp(self) -> None:
         self.test_client = UnitTestClient()
 
     async def test_set_null(self):
-        await self.test_client.set(
-            '/schema/',
-            {'type': 'null'}
-        )
+        await self.test_client.set('/schema/', {'type': 'null'})
 
         response = await self.test_client.get('/schema/')
         self.assertDictEqual(response, {'type': 'null'})
