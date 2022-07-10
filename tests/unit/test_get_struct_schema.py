d836625e46d98c9a9429b8560f64b7781be34301
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-10 13:18:40 +0200

    special chars

---
+++ tests/unit/test_get_struct_schema.py
@@ -0,0 +1,23 @@
+from unittest import IsolatedAsyncioTestCase
+
+from seizento.controllers.exceptions import Forbidden
+from tests.unit.unit_test_client import UnitTestClient
+
+
+class TestGetStructSchema(IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.test_client = UnitTestClient()
+
+    async def test_get_field_schema(self):
+        await self.test_client.set('/schema', {'type': 'object', 'properties': {'a': {'type': 'string'}}})
+
+        response = await self.test_client.get('/schema/a')
+
+        self.assertEqual({'type': 'string'}, response)
+
+    async def test_get_field_schema_with_special_chars(self):
+        await self.test_client.set('/schema', {'type': 'object', 'properties': {'^&*(2 .$': {'type': 'string'}}})
+
+        response = await self.test_client.get('/schema/%5E%26%2A%282%20.%24')
+
+        self.assertEqual({'type': 'string'}, response)
