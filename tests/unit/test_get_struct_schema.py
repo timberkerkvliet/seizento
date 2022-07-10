dbf366f049e5a717eb5ca5e126400cddff3a0940
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-07-10 13:21:23 +0200

    more tests

--- tests/unit/test_get_struct_schema.py
+++ tests/unit/test_get_struct_schema.py
@@ -21,3 +21,11 @@ class TestGetStructSchema(IsolatedAsyncioTestCase):
         response = await self.test_client.get('/schema/%5E%26%2A%282%20.%24')
 
         self.assertEqual({'type': 'string'}, response)
+
+    async def test_get_schema_after_setting_field_with_special_chars(self):
+        await self.test_client.set('/schema', {'type': 'object'})
+        await self.test_client.set('/schema/%5E%26%2A%282%20.%24', {'type': 'string'})
+
+        response = await self.test_client.get('/schema')
+
+        self.assertEqual({'type': 'object', 'properties': {'^&*(2 .$': {'type': 'string'}}}, response)
