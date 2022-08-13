a4314a536782b2fb67fa18c3d6b2f81bdacbe22c
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-08-13 19:36:52 +0200

    get schema tests

--- tests/unit/test_get_schema.py
+++ tests/unit/test_get_schema.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from seizento.controllers.exceptions import Unauthorized
+from seizento.controllers.exceptions import Unauthorized, NotFound
 from tests.unit.unit_test_client import UnitTestClient
 
 
@@ -8,40 +8,33 @@ class TestGetSchema(TestCase):
     def setUp(self) -> None:
         self.test_client = UnitTestClient()
 
-    def test_get_field_schema(self):
+    def test_get_property(self):
         self.test_client.set('/schema/test', {'type': 'object', 'properties': {'a': {'type': 'string'}}})
 
         response = self.test_client.get('/schema/test/a')
 
         self.assertEqual({'type': 'string'}, response)
 
-    def test_get_field_schema_with_special_chars(self):
+    def test_get_nonexistent_property(self):
+        self.test_client.set('/schema/test', {'type': 'object', 'properties': {'a': {'type': 'string'}}})
+
+        with self.assertRaises(NotFound):
+            self.test_client.get('/schema/test/b')
+
+    def test_get_property_with_special_chars(self):
         self.test_client.set('/schema/test', {'type': 'object', 'properties': {'^&*(2 .$': {'type': 'string'}}})
 
         response = self.test_client.get('/schema/test/%5E%26%2A%282%20.%24')
 
         self.assertEqual({'type': 'string'}, response)
 
-    def test_get_schema_after_setting_field_with_special_chars(self):
+    def test_get_schema_after_setting_property_with_special_chars(self):
         self.test_client.set('/schema/test', {'type': 'object'})
         self.test_client.set('/schema/test/%5E%26%2A%282%20.%24', {'type': 'string'})
 
         response = self.test_client.get('/schema/test')
 
         self.assertEqual({'type': 'object', 'properties': {'^&*(2 .$': {'type': 'string'}}}, response)
-
-
-    def test_multiple_types(self):
-        self.test_client.set(
-            'schema/test',
-            {
-                'type': ['array', 'object', 'string']
-            }
-        )
-
-        response = self.test_client.get('schema/test')
-
-        self.assertEqual({'array', 'object', 'string'}, set(response['type']))
 
     def test_empty(self):
         self.test_client.set('schema/test', {})
@@ -67,16 +60,16 @@ class TestGetSchema(TestCase):
     def test_empty_items(self):
         self.test_client.set('schema/test', {'items': {}})
 
-        response = self.test_client.get('schema/test')
+        response = self.test_client.get('schema/test/~items')
 
-        self.assertEqual({'items': {}}, response)
+        self.assertEqual({}, response)
 
     def test_empty_additional_properties(self):
         self.test_client.set('schema/test', {'additionalProperties': {}})
 
-        response = self.test_client.get('schema/test')
+        response = self.test_client.get('schema/test/~properties')
 
-        self.assertEqual({'additionalProperties': {}}, response)
+        self.assertEqual({}, response)
 
     def test_no_access(self):
         self.test_client.set(
