e0f9b9eaff435742d82bec4c729b8047040999fd
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-08-14 20:27:57 +0200

    simplify tests

--- tests/unit/test_general_errors.py
+++ tests/unit/test_general_errors.py
@@ -1,16 +1,12 @@
 from unittest import TestCase
 
-from seizento.controllers.exceptions import NotFound, BadRequest, MethodNotAllowed, Forbidden
+from seizento.controllers.exceptions import BadRequest
 from tests.unit.unit_test_client import UnitTestClient
 
 
 class TestGeneralErrors(TestCase):
     def setUp(self) -> None:
         self.test_client = UnitTestClient()
-
-    def test_when_setting_schema_with_no_paren_then_raise_not_found(self):
-        with self.assertRaises(NotFound):
-            self.test_client.set('/schema/test/a', {'type': 'integer'})
 
     def test_when_getting_nonsensical_resource_then_raise_bad_request(self):
         with self.assertRaises(BadRequest):
@@ -19,9 +15,3 @@ class TestGeneralErrors(TestCase):
     def test_when_getting_non_existing_base_type_then_raise_bad_request(self):
         with self.assertRaises(BadRequest):
             self.test_client.get('/hoi-daar-allemaal')
-
-    def test_set_empty_type_data(self):
-        try:
-            self.test_client.set('/schema/test', {})
-        except BadRequest:
-            self.fail()
